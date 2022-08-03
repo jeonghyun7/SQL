fb240753a62de5b0a74c0e5dc8420c4f544c083f
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-08-03 18:26:46 +0900

    Add files via upload

---
+++ 0803/테이블과뷰_테이블생성.sql
@@ -0,0 +1,50 @@
+--------SQL로 테이블생성-------------------------------------------
+USE test;
+DROP TABLE if EXISTS usertbl;
+CREATE TABLE usertbl (
+	userID CHAR(8) NOT NULL PRIMARY KEY,
+	name VARCHAR(10) NOT NULL,
+	birthYear INT NOT NULL,
+	addr CHAR(2) NOT NULL,
+	mobile1 CHAR(3) NULL,
+	mobile2 CHAR(8) NULL,
+	height SMALLINT NULL,
+	mDate DATE NULL
+);
+
+USE test;
+INSERT INTO usertbl VALUES ('LSG', '이승기', 1987, '서울', '011', '11111111', 182, '2008-8-8');
+INSERT INTO usertbl VALUES ('KBS', '김범수', 1979, '경남', '011', '22222222', 173, '2012-4-4');
+INSERT INTO usertbl VALUES ('KKH', '김경호', 1971, '전남', '019', '33333333', 177, '2007-7-7');
+INSERT INTO usertbl VALUES ('JYP', '조용필', 1950, '경기', '011', '44444444', 166, '2009-4-4');
+INSERT INTO usertbl VALUES ('SSK', '성시경', 1979, '서울', NULL, NULL, 186, '2013-12-12');
+INSERT INTO usertbl VALUES ('LJB', '임재범', 1963, '서울', '016', '66666666', 182, '2009-9-9');
+INSERT INTO usertbl VALUES ('YJS', '윤종신', 1969, '경남', NULL, NULL, 170, '2005-5-5');
+INSERT INTO usertbl VALUES ('EJW', '은지원', 1978, '경북', '011', '88888888', 174, '2014-3-3');
+INSERT INTO usertbl VALUES ('JKW', '조관우', 1965, '경기', '018', '99999999', 172, '2013-5-5');
+INSERT INTO usertbl VALUES ('BBK', '바비킴', 1973, '서울', '010', '00000000', 176, '2013-5-5');
+
+USE test;
+DROP TABLE if EXISTS buytbl;
+CREATE TABLE buytbl (
+	num INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	userID CHAR(8) NOT NULL,
+	prodName CHAR(6) NOT NULL,
+	groupName CHAR(4) NULL,
+	price INT NOT NULL,
+	amount SMALLINT NOT null
+	, FOREIGN KEY (userID) REFERENCES usertbl (userID)
+);
+
+INSERT INTO buytbl VALUES (NULL, 'KBS', '운동화', NULL, 30, 2);
+INSERT INTO buytbl VALUES (NULL, 'KBS', '노트북', '전자', 1000, 1);
+INSERT INTO buytbl VALUES (NULL, 'JYP', '모니터', '전자', 200, 1);
+INSERT INTO buytbl VALUES (NULL, 'BBK', '모니터', '전자', 200, 5);
+INSERT INTO buytbl VALUES (NULL, 'KBS', '청바지', '의류', 50, 3);
+INSERT INTO buytbl VALUES (NULL, 'BBK', '메모리', '전자', 80, 10);
+INSERT INTO buytbl VALUES (NULL, 'SSK', '책', '서적', 15, 5);
+INSERT INTO buytbl VALUES (NULL, 'EJW', '책', '서적', 15, 2);
+INSERT INTO buytbl VALUES (NULL, 'EJW', '청바지', '의류', 50, 1);
+INSERT INTO buytbl VALUES (NULL, 'BBK', '운동화', NULL, 30, 2);
+INSERT INTO buytbl VALUES (NULL, 'EJW', '책', '서적', 15, 1);
+INSERT INTO buytbl VALUES (NULL, 'BBK', '운동화', NULL, 30, 2);
